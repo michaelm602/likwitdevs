e0f4a50ff6aaaa65463f1d627ee53251939d02e8
michaelm602 <48140179+michaelm602@users.noreply.github.com>
2025-08-27 11:01:39 -0700

    vite config adjustment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/likwitdevs/',   // must match repo name
+  base: '/',   // must match repo name
 })
