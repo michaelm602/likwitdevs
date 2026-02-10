945ae3b8f98c37d6880e436a2c8431bd419023ec
michaelm602 <48140179+michaelm602@users.noreply.github.com>
2026-02-10 09:06:28 -0800

    vite config base fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/likwitdevs/",   // must match repo name
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE || "/",
+  };
+});
