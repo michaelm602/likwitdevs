e3882ee0fe9891e10902641db58c91f498c7f7ad
michaelm602 <48140179+michaelm602@users.noreply.github.com>
2026-02-10 07:24:52 -0800

    removed some console logs

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,12 +4,6 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-console.log("ENV CHECK", {
-    apiKey: import.meta.env.VITE_FB_API_KEY,
-    authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FB_PROJECT_ID,
-});
-
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FB_API_KEY,
     authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
